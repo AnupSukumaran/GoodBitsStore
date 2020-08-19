3ac9ccc1106162b1ecf699c9101c8f3017019374
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-08-19 10:21:40 +0530

    - layout corrections

--- GoodBitsStore/ViewControllers/HomePickUpLocTableViewCell.swift
+++ GoodBitsStore/ViewControllers/HomePickUpLocTableViewCell.swift
@@ -24,66 +24,42 @@ class HomePickUpLocTableViewCell: UITableViewCell {
         }
     }
     
-//    var currentLoc: CLLocation? {
-//        didSet {
-//            getDistancevalue()
-//        }
-//    }
-    
-//    override class func awakeFromNib() {
-//
-//        super.awakeFromNib()
-//
-//    }
-//
+
     func cellModelConfig() {
         guard let model = cellModel else {return}
-        
+        hideLabelsIfNoData(in: model.pickup)
         lbPickAlias.text = model.pickup.alias ?? ""
-        Logger.p("model.pickup.address2- \(model.pickup.address2 ?? "NO data")")
-        let comma = (model.pickup.address1 == "") ? "" : ", "
-        let add2 = (model.pickup.address2 == "") ? "" : (comma + (model.pickup.address2 ?? ""))
-        lbAddress.text = (model.pickup.address1 ?? "") + add2
+        
+//        let comma = (model.pickup.address1 == "") ? "" : ", "
+//        let add2 = (model.pickup.address2 == "") ? "" : (comma + (model.pickup.address2 ?? ""))
+        lbAddress.text = settingAddresTxt(model.pickup)//(model.pickup.address1 ?? "") + add2
         lbCity.text = model.pickup.city ?? ""
-        if let d = model.pickup.distance {
-            lbKm.isHidden = false
-            lbKm.text = d.toDecimalsStr(2) + " km"
-        } else {
-            lbKm.isHidden = true
-        }
+        lbKm.text = setDistanceVal(model.pickup)
+//        if let d = model.pickup.distance {
+//            lbKm.text = d.toDecimalsStr(2) + " km"
+//        }
     }
     
-//    func getDistancevalue() {
-//        guard let d = currentLoc else {lbKm.isHidden = true;return}
-//        lbKm.isHidden = false
-//        guard let lat = cellModel?.pickup.latitude , let long = cellModel?.pickup.longitude else {return}
-//        let distance = getDistance(coord1: (lat: d.coordinate.latitude, long: d.coordinate.longitude), coord2: (lat: lat, long: long)).toDecimalsStr(2)
-//        lbKm.text = distance + "km"
-//    }
+    func setDistanceVal(_ pickUp: Pickup) -> String {
+        var dist = ""
+        if let d = pickUp.distance {
+            dist = d.toDecimalsStr(2) + " km"
+        }
+        return dist
+    }
     
-//    override func layoutIfNeeded() {
-//        super.layoutIfNeeded()
-//        clipsToBounds = false
-//        contentView.clipsToBounds = false
-//    }
-//
-//    override func layoutSubviews() {
-//        super.layoutSubviews()
-//      //  contentView.clipsToBounds = true
-//        clipsToBounds = false
-//        contentView.clipsToBounds = false
-//    }
-//
-//
-//    override func prepareForReuse() {
-//      //  contentView.clipsToBounds = true
-//        clipsToBounds = false
-//        contentView.clipsToBounds = false
-//        lbPickAlias.text = ""
-//        lbAddress.text = ""
-//        lbCity.text = ""
-//        lbKm.text = ""
-//    }
+    func hideLabelsIfNoData(in pickUp: Pickup){
+        lbPickAlias.isHidden = (pickUp.alias ?? "") == ""
+        lbAddress.isHidden = settingAddresTxt(pickUp) == ""
+        lbCity.isHidden = (pickUp.city ?? "") == ""
+        lbKm.isHidden = pickUp.distance == nil
+    }
+    
+    func settingAddresTxt(_ pickUp: Pickup) -> String {
+        let comma = (pickUp.address1 == "") ? "" : ", "
+        let add2 = (pickUp.address2 == "") ? "" : (comma + (pickUp.address2 ?? ""))
+        return (pickUp.address1 ?? "") + add2
+    }
     
 
 }
